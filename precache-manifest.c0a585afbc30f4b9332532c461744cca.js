b196a1647be33fdfb80b5d14663622f2ba49b831
essennejaye <sjeffers02@gmail.com>
2020-10-18 01:14:51 -0700

    Updates

---
+++ precache-manifest.c0a585afbc30f4b9332532c461744cca.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1eeb919de1fe9468663f5631d2af35",
+    "url": "/webdev-showcase/index.html"
+  },
+  {
+    "revision": "5f9d0e5d57e186b1c34c",
+    "url": "/webdev-showcase/static/css/main.1738006d.chunk.css"
+  },
+  {
+    "revision": "6b07c5c742f1b0ad863a",
+    "url": "/webdev-showcase/static/js/2.362339db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webdev-showcase/static/js/2.362339db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f9d0e5d57e186b1c34c",
+    "url": "/webdev-showcase/static/js/main.7392f341.chunk.js"
+  },
+  {
+    "revision": "f74a72a260194e30a88d",
+    "url": "/webdev-showcase/static/js/runtime-main.794075a6.js"
+  },
+  {
+    "revision": "ea9675e08887cd57fb2e9283f246f64f",
+    "url": "/webdev-showcase/static/media/Orange-Background-Wallpaper-HD-16464.ea9675e0.png"
+  },
+  {
+    "revision": "5dc6d17f12a1e248bdf12736d0376a03",
+    "url": "/webdev-showcase/static/media/my-schtick-kick.5dc6d17f.png"
+  },
+  {
+    "revision": "4e10929b14c2b7f499edabf0f1a47ab4",
+    "url": "/webdev-showcase/static/media/profile_pic.4e10929b.jpg"
+  },
+  {
+    "revision": "afe9466b71982d547d38f064b02f3f83",
+    "url": "/webdev-showcase/static/media/rv-roadtrip.afe9466b.png"
+  },
+  {
+    "revision": "e31c1624fd0b7aa866dfea6ab6a7644e",
+    "url": "/webdev-showcase/static/media/weather-dashboard.e31c1624.png"
+  },
+  {
+    "revision": "88dcc59d571cf45a95cf636b79b0bc5c",
+    "url": "/webdev-showcase/static/media/web-dev-op-ed.88dcc59d.png"
+  },
+  {
+    "revision": "b9f273909fa305cc2280a6c326a05e2f",
+    "url": "/webdev-showcase/static/media/workday-scheduler.b9f27390.png"
+  }
+]);
