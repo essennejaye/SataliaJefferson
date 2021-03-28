1db31bc6f896361de3414958967d5353786523a3
essennejaye <sjeffers02@gmail.com>
2021-03-28 11:22:56 -0700

    resume updated

--- src/componets/Resume/index.js
+++ src/componets/Resume/index.js
@@ -4,7 +4,6 @@ const Resume = () => {
   return (
     <section className="section-container">
       <div className="my-2">
-        {/* <iframe title='My Resume' src="https://drive.google.com/file/d/18MynqDzX489JtVmAh6q-bdxv1qxPY2gu/preview" ></iframe> */}
         <iframe title='My Resume' src="https://drive.google.com/file/d/1dmqtBk3nzYq0qVCeGLguMaMnlZRmfwyl/preview"></iframe>
       </div>
     </section>
